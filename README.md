e5b2866baecfa32b1f83925297488383dd6d8bcc
mikehodgson <mhodgson@gmail.com>
2024-06-21 10:28:43 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-furbcp
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/mikehodgson/vitejs-vite-furbcp)
---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup></script>
+
+<template>
+  <h1>Hello VueUse!</h1>
+  <a href="https://vueuse.org/" target="_blank">Documentation</a>
+</template>
+
+<style></style>
